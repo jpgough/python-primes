0a8d32effa370acb135727690a20a3b3f78518cb
Jim Gough <jpgough@gmail.com>
2015-02-20 16:12:27 -0500

    Fix 1 as not prime

--- Prime.py
+++ Prime.py
@@ -1,12 +1,10 @@
 class Prime():
 	def isPrime(self, number):
-		if number == 1 or number == 2:
-			return True
-		if number < 1 or number % 2 == 0:
-			return False
-		
-		for n in range(number-1, 0):
+		if number <= 1 or number % 2 == 0:
+			return number == 2
+
+		for n in range(number - 1, 0):
 			if n % number == 0:
 				return False
-		
+
 		return True
